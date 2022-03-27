33818d63d58314a089caff188c85eb7f3cce7b13
99010912Tamicktom@ <rickpokemon38@gmail.com>
2022-03-27 20:33:24 -0300

    atualizado um pouco do visual

---
+++ scripts/style.js
@@ -0,0 +1,14 @@
+const main = document.querySelector("#main");
+const search_div = document.querySelector("#search_div");
+
+function showSearchResult() {
+    if (main.classList.contains('invisible')) {
+        main.classList.remove('invisible');
+        main.style.width = '100%';
+        search_div.style.width = '0%';
+        search_div.style.display = 'none';
+    }
+    search_div.style.display = "none";
+    //document.querySelector("#main").classList = "main";
+}
+//showSearchResult();
