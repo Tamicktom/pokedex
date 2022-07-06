11a3d028c599c739104dea4e2e02ae3014193dae
99010912Tamicktom@ <rickpokemon38@gmail.com>
2022-07-06 00:17:49 -0300

    update to background things

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,47 @@
-function loadpkm(name) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${name}/`;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            // document.getElementById("Nome").innerHTML = data['name'];
-            // document.getElementById("Número").innerHTML = data['id'];
+//* @note Funções auxiliares
 
-            select('#pkm_name').innerHTML = (data['name']).toUpperCase();
-            let img = data.sprites.other["official-artwork"].front_default;
-            document.getElementById("pkm_img").src = img;
-        })
-        .catch((erro) => {
-            alert(`${name} é inválido`);
-        })
-
-}
-function SearchPkmByName() {
-    const pkmName = document.getElementById("pkm_search_input").value.toLowerCase();
-    loadpkm(pkmName);
-}
-
-document.getElementById("pkm_search_button").onclick = SearchPkmByName;
-
-
-//* Funções auxiliares
-function render(obj, text, add) {
+const render = (obj, text, add) => {
     if (typeof obj === 'string') obj = document.querySelector(obj);
     add === true ? obj.innerHTML += text : obj.innerHTML = text;
 }
 
-function select(obj) {
+const select = (obj) => {
     return document.querySelector(obj);
+}
+
+//* @note api work
+async function loadPkm(name) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}/`;
+
+    fetch(url)
+        .then(response => { return response.json() })
+        .then(data => {
+            console.log(data);
+            showSarch(data);
+        })
+        .catch((erro) => {
+            alert(`${name} é inválido`);
+        })
+}
+
+function SearchPkm() {
+    const pkmName = (select("#pkm_search_input").value).replace(/\s/g, '');
+    loadPkm(pkmName.toLowerCase());
+}
+select("#pkm_search_button").onclick = SearchPkm;
+
+
+//* @note Show api search result
+function showSarch(data) {
+    select("#pkm_img").src = data.sprites.other["official-artwork"].front_default;
+
+    render('#pkm_name', (data.name).toUpperCase());
+    data.types.forEach(type => {
+        render(
+            '#pkm_type',
+            `<span class="type ${type.type.name}">${(type.type.name).toUpperCase()}</span>`,
+            true);
+    });
+    
+    render('#pkm_id', `Nº ${data.id}`);
 }
--- scripts/style.js
+++ scripts/style.js
@@ -1,57 +1,95 @@
-function updatePkmTypeColors() {
-    const pkm_types_colors = {
-        "Normal": "var(--Normal)",
-        "Lutador": "var(--Lutador)",
-        "Voador": "var(--Voador)",
-        "Venenoso": "var(--Venenoso)",
-        "Terrestre": "var(--Terrestre)",
-        "Pedra": "var(--Pedra)",
-        "Inseto": "var(--Inseto)",
-        "Fantasma": "var(--Fantasma)",
-        "Metal": "var(--Metal)",
-        "Fogo": "var(--Fogo)",
-        "Água": "var(--Água)",
-        "Grama": "var(--Grama)",
-        "Elétrico": "var(--Elétrico)",
-        "Psiquico": "var(--Psiquico)",
-        "Gelo": "var(--Gelo)",
-        "Dragão": "var(--Dragão)",
-        "Trevas": "var(--Trevas)",
-        "Fada": "var(--Fada)",
-        "Desconhecido": "var(--Desconhecido)",
-        "Sombras": "var(--Sombras)"
-    };
-    const type1 = document.querySelector("#pkm_type1");
-    const type2 = document.querySelector("#pkm_type2");
-    const keys = Object.keys(pkm_types_colors);
-    keys.forEach((key) => {
-        if (key.toLowerCase() == type1.innerHTML.toLowerCase())
-            document.documentElement.style.setProperty('--pokemon-color-theme', pkm_types_colors[key]);
-        if (key.toLowerCase() == type2.innerHTML.toLowerCase())
-            document.documentElement.style.setProperty('--pokemon-color-theme2', pkm_types_colors[key]);
+function inicialization() {
+    const screen = select("#screen");
+    const saveScreenStyle = screen.style;
+    screen.style.flexDirection = "column";
+    screen.style.transition = "all 1.7s ease-in-out";
+    screen.style.background = "rgba(0,0,150,0.5)";
+    screen.style.color = "white";
 
-    });
-    if (type2.innerHTML.toLowerCase() == '') {
-        document.documentElement.style.setProperty('--pokemon-color-theme2', '--pokemon-color-theme');
-    }
+
+    tittle = "<h1>Pokédex</h1>";
+    subtittle = "<p>Pokedex made with data from pokeapi</p>";
+    render('#screen', tittle);
+    render('#screen', subtittle, true);
+
+
+
+    //screen.style = saveScreenStyle;
 }
 
-function showSearchResult() {
-    const main = document.querySelector("#main");
-    const search_div = document.querySelector("#search_div");
-
-    if (main.classList.contains('invisible')) {
-        main.classList.remove('invisible');
-        main.style.width = '100%';
-        search_div.style.width = '0%';
-        search_div.style.display = 'none';
-    }
-    search_div.style.display = "none";
-}
+//inicialization();
 
 
-showSearchResult();
-updatePkmTypeColors();
-let document_Style = getComputedStyle(document.documentElement);
-let myVal = document_Style.getPropertyValue('--Dragão');
-console.log(myVal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function updatePkmTypeColors() {
+//     const pkm_types_colors = {
+//         "Normal": "var(--Normal)",
+//         "Lutador": "var(--Lutador)",
+//         "Voador": "var(--Voador)",
+//         "Venenoso": "var(--Venenoso)",
+//         "Terrestre": "var(--Terrestre)",
+//         "Pedra": "var(--Pedra)",
+//         "Inseto": "var(--Inseto)",
+//         "Fantasma": "var(--Fantasma)",
+//         "Metal": "var(--Metal)",
+//         "Fogo": "var(--Fogo)",
+//         "Água": "var(--Água)",
+//         "Grama": "var(--Grama)",
+//         "Elétrico": "var(--Elétrico)",
+//         "Psiquico": "var(--Psiquico)",
+//         "Gelo": "var(--Gelo)",
+//         "Dragão": "var(--Dragão)",
+//         "Trevas": "var(--Trevas)",
+//         "Fada": "var(--Fada)",
+//         "Desconhecido": "var(--Desconhecido)",
+//         "Sombras": "var(--Sombras)"
+//     };
+//     const type1 = document.querySelector("#pkm_type1");
+//     const type2 = document.querySelector("#pkm_type2");
+//     const keys = Object.keys(pkm_types_colors);
+//     keys.forEach((key) => {
+//         if (key.toLowerCase() == type1.innerHTML.toLowerCase())
+//             document.documentElement.style.setProperty('--pokemon-color-theme', pkm_types_colors[key]);
+//         if (key.toLowerCase() == type2.innerHTML.toLowerCase())
+//             document.documentElement.style.setProperty('--pokemon-color-theme2', pkm_types_colors[key]);
+
+//     });
+//     if (type2.innerHTML.toLowerCase() == '') {
+//         document.documentElement.style.setProperty('--pokemon-color-theme2', '--pokemon-color-theme');
+//     }
+// }
+
+// function showSearchResult() {
+//     const main = document.querySelector("#main");
+//     const search_div = document.querySelector("#search_div");
+
+//     if (main.classList.contains('invisible')) {
+//         main.classList.remove('invisible');
+//         main.style.width = '100%';
+//         search_div.style.width = '0%';
+//         search_div.style.display = 'none';
+//     }
+//     search_div.style.display = "none";
+// }
+
+
+// showSearchResult();
+// updatePkmTypeColors();
+// let document_Style = getComputedStyle(document.documentElement);
+// let myVal = document_Style.getPropertyValue('--Dragão');
+// console.log(myVal);
